20a7375b69df2c366ae9df257c9ad651bf48e236
横茶横葉 <115348123+Yokokiri-Cha@users.noreply.github.com>
2023-03-21 10:24:57 +0900

    Add files via upload

---
+++ Backup/利用開始/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace 利用開始
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.Location = new System.Drawing.Point(335, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(317, 387);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Sony Music Center For PC 音楽再生ソフト を起動";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 584);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "A.S.K";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkCyan;
+            this.button2.Location = new System.Drawing.Point(658, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(317, 387);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Microsoft Edge web browser を起動";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Honeydew;
+            this.button3.Location = new System.Drawing.Point(981, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(317, 387);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Any Video Converter 動画変換ソフト を起動";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Honeydew;
+            this.button5.Location = new System.Drawing.Point(1304, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(317, 387);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "First Lock 画面ロックソフト を起動";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 118);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Station TV をご覧になるにはパスワードを入力して下さい。\r\nよろしければここをクリックして下さい。";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 263);
+            this.textBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1630, 408);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "クイック起動ランチャーfor片桐家";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Backup/利用開始/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 利用開始
+{
+    public partial class Form1 : Form
+    {
+        string stp ="kwyny";
+      
+        private System.Diagnostics.Process 利用 = new System.Diagnostics.Process();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = "C:\\Program Files (x86)\\StationTV X - コピー (2) - コピー.lnk";
+                利用.StartInfo.WorkingDirectory = "C:\\Program Files (x86)\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = ".\\lis audio player t.exe";
+                利用.StartInfo.WorkingDirectory = ".\\lis\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = ".\\ドキュメント作成2019.exe";
+                利用.StartInfo.WorkingDirectory = ".\\D2019\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = ".\\写真取り込み.exe";
+                利用.StartInfo.WorkingDirectory = ".\\\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = "C:\\Program Files (x86)\\Sony\\Music Center\\MusicCenter.exe";
+                利用.StartInfo.WorkingDirectory = "C:\\Program Files (x86)\\Sony\\Music Center\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = ".\\Picsh.exe";
+                利用.StartInfo.WorkingDirectory = ".\\Picsh\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("このソフトウェアは他のプログラムがないと作動できないプログラムです。");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = ".\\アラームメッセージv3.exe";
+                利用.StartInfo.WorkingDirectory = ".\\アラームメッセージ\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = ".\\毎日日記1.exe";
+                利用.StartInfo.WorkingDirectory = ".\\NK\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = "C:\\Program Files (x86)\\Microsoft Edge.lnk";
+                利用.StartInfo.WorkingDirectory = "C:\\Program Files (x86)\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = "C:\\Program Files (x86)\\Anvsoft\\Any Video Converter\\AVCFree.exe";
+            
+                利用.StartInfo.WorkingDirectory = "C:\\Program Files (x86)\\Anvsoft\\Any Video Converter\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                利用.StartInfo.FileName = "C:\\Users\\askke\\Desktop\\Fast Lock.exe";
+
+                利用.StartInfo.WorkingDirectory = "C:\\Users\\askke\\Desktop\\";
+                利用.Start();
+                //Myexe.WaitForExit();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            if (stp == textBox1.Text)
+            {
+                try
+                {
+                    利用.StartInfo.FileName = "C:\\Program Files (x86)\\StationTV X - コピー (2) - コピー.lnk";
+                    利用.StartInfo.WorkingDirectory = "C:\\Program Files (x86)\\";
+                    利用.Start();
+                    //Myexe.WaitForExit();
+                }
+                catch
+                {
+                    MessageBox.Show("エラーが発生しました。プログラムを開けません。");
+                }
+            }
+            else
+            {
+                MessageBox.Show("パスワードが一致しませんでした。入力された文字列をもう一度ご確認ください。");
+            }
+        }
+    }
+}
+
